7a6d7d3bd653290e360519045cc1862dbe1455e1
0xnivek <0xnivek@gmail.com>
2021-12-20 12:35:45 -0500

    Init flash loan contracts

---
+++ hardhat/contracts/JoeFlashloanBorrower.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "./ERC3156FlashLenderInterface.sol";
+import "./ERC3156FlashBorrowerInterface.sol";
+
+interface Comptroller {
+    function isMarketListed(address cTokenAddress) external view returns (bool);
+}
+
+interface ERC20 {
+    function approve(address spender, uint256 amount) external;
+}
+
+// FlashloanBorrower is a simple flashloan Borrower implementation for testing
+contract JoeFlashloanBorrower is ERC3156FlashBorrowerInterface {
+    /**
+     * @notice C.R.E.A.M. comptroller address
+     */
+    address public comptroller;
+
+    constructor(address _comptroller) {
+        comptroller = _comptroller;
+    }
+
+    function doFlashloan(
+        address flashloanLender,
+        address borrowToken,
+        uint256 borrowAmount
+    ) external {
+        bytes memory data = abi.encode(borrowToken, borrowAmount);
+        ERC3156FlashLenderInterface(flashloanLender).flashLoan(
+            this,
+            borrowToken,
+            borrowAmount,
+            data
+        );
+    }
+
+    function onFlashLoan(
+        address initiator,
+        address token,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata data
+    ) external override returns (bytes32) {
+        require(
+            Comptroller(comptroller).isMarketListed(msg.sender),
+            "untrusted message sender"
+        );
+        require(
+            initiator == address(this),
+            "FlashBorrower: Untrusted loan initiator"
+        );
+        (address borrowToken, uint256 borrowAmount) = abi.decode(
+            data,
+            (address, uint256)
+        );
+        require(
+            borrowToken == token,
+            "encoded data (borrowToken) does not match"
+        );
+        require(
+            borrowAmount == amount,
+            "encoded data (borrowAmount) does not match"
+        );
+        ERC20(token).approve(msg.sender, amount + fee);
+        // your logic is written here...
+        return keccak256("ERC3156FlashBorrowerInterface.onFlashLoan");
+    }
+}
